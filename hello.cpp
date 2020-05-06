cf7b4c1f15ad9adf181beb0c0e125de40b4ccdf6
Jackie Allex <jackieallex@gmail.com>
2020-05-06 10:47:02 -0400

    Initial commit and PPM color output

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout <<"enter you name: ";
+    string name;
+    cin >> name;
+    cout <<"Hello " << name;
+    cout << "\n";
+    return 0;
+
+}
